1c7faff21870f871085189b8e714cbab8447b88f
Satyaprakash Maurya <satyaprakashmaurya756@gmail.com>
2026-01-03 19:14:38 +0530

    datatype

--- dataType/index.js
+++ dataType/index.js
@@ -1,8 +1,29 @@
-console.log("Hellobeta ji");
+//data type
+//Number
+let age = 18;
 
-let a = 5;
+//console.log("age", age);
 
-console.log("print the a ", a);
-//zgdufgsdjkuagefjwe
-let satya = 3;
-console.log("print the satya", satya);
+let price = 34;
+//console.log("price", price);
+
+// string
+
+let name = "satya";
+let city = "Noida";
+//console.log("name ,city", name, city);
+
+// bolean
+
+let isLoggedIn = false;
+
+if (isLoggedIn) {
+  //console.log("Welcome User");
+} else {
+  //console.log("Please Login");
+}
+
+let age = 20;
+let isAdult = age >= 18;
+
+console.log(isAdult); // true
